73e7c7b85beb63000efb6385fd71e86af3689da7
Santhyamoorthi <santhiyamoorthi455@gmail.com>
2025-10-06 11:21:21 +0530

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,17 @@
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
+    return (
+        <>
+            <nav className="flex p-4 bg-base-100 text-base-content">
+                <div>
+                    <h1 className='text-xl font-bold text-primary lg:text-4xl'>
+                        <a href="/">
+                            <span>Shadow-Shopper</span>
+                        </a>
+                    </h1>
+                </div>
+            </nav>
+        </>
+    )
+}
 export default Navbar
