689406bfe44e26364fb4dd52fe3d713d978efb33
Santhyamoorthi <santhiyamoorthi455@gmail.com>
2025-10-06 11:28:34 +0530

    done navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,11 @@ const Navbar = () => {
     return (
         <>
             <nav className="flex p-4 bg-base-100 text-base-content">
-                <div>
-                    <h1 className='text-xl font-bold text-primary lg:text-4xl'>
-                        <a href="/">
-                            <span>Shadow-Shopper</span>
-                        </a>
-                    </h1>
-                </div>
+                <h1 className='text-xl font-bold text-primary lg:text-4xl'>
+                    <a href="/">
+                        <span>Shadow-Shopper</span>
+                    </a>
+                </h1>
             </nav>
         </>
     )
